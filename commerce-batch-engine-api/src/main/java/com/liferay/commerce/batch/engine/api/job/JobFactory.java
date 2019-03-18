matijapetanjek/com-liferay-commerce

COMMERCE-1067 - commerce-batch-engine-api - Define API interfaces and base classes

commerce-batch/commerce-batch-engine-api/src/main/java/com/liferay/commerce/batch/engine/api/job/JobFactory.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.batch.engine.api.job;
+
+import com.liferay.commerce.batch.engine.api.item.ItemReader;
+import com.liferay.commerce.batch.engine.api.item.ItemWriter;
+
+/**
+ * @author Ivica Cardic
+ */
+public interface JobFactory {
+
+	public Job create(
+		String name, ItemReader itemReader, ItemWriter itemWriter);
+
+	public Job create(
+		String id, String name, ItemReader itemReader, ItemWriter itemWriter);
+
+}

ivicac/liferay-portal

commerce-batch-engine-api/src/main/java/com/liferay/commerce/batch/engine/api/job/JobFactory.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.batch.engine.api.job;
+
+import com.liferay.commerce.batch.engine.api.item.ItemReader;
+import com.liferay.commerce.batch.engine.api.item.ItemWriter;
+
+/**
+ * @author Ivica Cardic
+ */
+public interface JobFactory {
+
+	public Job create(
+		String name, ItemReader itemReader, ItemWriter itemWriter);
+
+	public Job create(
+		String key, String name, ItemReader itemReader, ItemWriter itemWriter);
+
+}